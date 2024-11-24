224aecf02e1d8953252cc7692ebd5fa5164a509b
yapabandara_7 <96159272+dhanuyapa@users.noreply.github.com>
2024-11-25 03:48:42 +0530

    add icon

--- fetch.php
+++ fetch.php
@@ -9,9 +9,16 @@ $result = $conn->query($sql);
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="css/fetch.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kidney Patients Data</title>
+
     <style>
         table {
             border-collapse: collapse;
@@ -32,13 +39,13 @@ $result = $conn->query($sql);
     </style>
 </head>
 <body>
-    <h1>Kidney Patients Data</h1>
+<div class="Title">Kidney Needer Details</div>
 
     <?php if ($result->num_rows > 0): ?>
         <table>
             <thead>
-                <tr>
-                    <th>First Name</th>
+                <tr style="header">
+                    <th >First Name</th>
                     <th>Last Name</th>
                     <th>Date of Birth</th>
                     <th>Gender</th>
@@ -63,8 +70,16 @@ $result = $conn->query($sql);
                         <td><?php echo htmlspecialchars($row['address']); ?></td>
                         <td><?php echo htmlspecialchars($row['nic']); ?></td>
                         <td>
-                            <a href="edit.php?nic=<?php echo $row['nic']; ?>">Edit</a> |
-                            <a href="delete.php?nic=<?php echo $row['nic']; ?>" onclick="return confirm('Are you sure you want to delete this record?');">Delete</a>
+                            <a href="edit.php?nic=<?php echo $row['nic']; ?>">  
+                                
+                            <div class="edit-image">
+            <img src="edit-16.png" alt="Yoga Image">
+        </div></a> 
+                            <a href="delete.php?nic=<?php echo $row['nic']; ?>" onclick="return confirm('Are you sure you want to delete this record?');">
+                                
+                            <div class="edit-image">
+            <img src="edit.png" alt="Yoga Image">
+        </div></a>
                         </td>
                     </tr>
                 <?php endwhile; ?>
